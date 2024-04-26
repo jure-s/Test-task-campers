1e4490086916dbd3468c227aab8fc10cd03f9f70
jure-s <71380578+jure-s@users.noreply.github.com>
2024-04-26 23:05:41 +0300

    404

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./index.css";
 import App from "./App";
 import { store } from "./store";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,6 +12,9 @@ root.render(
   </Provider>
 );
 
+<BrowserRouter basename="/testtest">
+  <App />
+</BrowserRouter>;
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
